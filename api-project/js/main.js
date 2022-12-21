7affe1129ca7b1440d1653d75a787d68f004fda9
AdamLyun <adamlyun@gmail.com>
2022-12-21 12:36:41 -0500

    api part 2

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,10 +1,11 @@
 import "../styles/style.css";
-const URL = "https;//api.quotable.io/random";
+const URL = "https: //api.quotable.io/random";
+
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log(data.content);
+document.getElementById("api-response").textContent =data;
   } catch (error) {
     console.log(error);
   }
