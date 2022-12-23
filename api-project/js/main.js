b3acfefee9c36651372c6a6d54bcf734086a669f
AdamLyun <adamlyun@gmail.com>
2022-12-23 12:43:09 -0500

    api part 4

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,11 +1,14 @@
 import "../styles/style.css";
-const URL = "https://api.quotable.io/random";
+const URL = "https://bible-api.com/john`+4:3";
+const named = document.getElementById("name");
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-document.getElementById("api-response").textContent =data.content;
+document.getElementById("api-response").textContent =data.reference ;
+document.getElementById("api-response2").textContent =data.text ;
+console.log(named.value)
   } catch (error) {
     console.log(error);
   }
