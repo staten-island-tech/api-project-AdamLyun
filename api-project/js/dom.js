2d57a43453162f64be2c5b6fecf86a401c2c88ad
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-01-01 11:12:21 -0500

    api part 5

---
+++ api-project/js/dom.js
@@ -0,0 +1,4 @@
+const DOMSelectors = {
+    button: document.getElementById("btn"),
+};
+export {DOMSelectors};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,16 +1,29 @@
 import "../styles/style.css";
-const URL = "https://bible-api.com/john`+4:3";
-const named = document.getElementById("name");
+import { DOMSelectors } from "./dom";
+
+
+
+DOMSelectors.button.addEventListener("click", function(){
+  const book=  document.getElementById("Name").value;
+  const chapter =  document.getElementById("Chapter").value;
+  const verse= document.getElementById("Verse").value;
+ const URL = `https://bible-api.com/${book}+${chapter}:${verse}`;
+getData(URL);
+
+console.log(URL);
+}
+);
+
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-document.getElementById("api-response").textContent =data.reference ;
-document.getElementById("api-response2").textContent =data.text ;
-console.log(named.value)
+document.getElementById("api-response").textContent = data.reference ;
+document.getElementById("api-response2").textContent = data.text ;
+
   } catch (error) {
     console.log(error);
   }
 }
-getData(URL);
+
