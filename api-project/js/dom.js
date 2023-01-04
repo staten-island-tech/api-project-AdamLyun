b2729e8f349658280f00b6d41de5f4160d8bf182
AdamLyun <adamlyun@gmail.com>
2023-01-04 13:15:26 -0500

    api part 7

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,4 +1,5 @@
 const DOMSelectors = {
     button: document.getElementById("btn"),
+    button2: document.getElementById("btn2"),
 };
 export {DOMSelectors};
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,29 +1,47 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-
-
-DOMSelectors.button.addEventListener("click", function(){
-  const book=  document.getElementById("Name").value;
-  const chapter =  document.getElementById("Chapter").value;
-  const verse= document.getElementById("Verse").value;
- const URL = `https://bible-api.com/${book}+${chapter}:${verse}`;
-getData(URL);
-
-console.log(URL);
-}
-);
-
+DOMSelectors.button.addEventListener("click", function () {
+  const book = document.getElementById("Name").value;
+  const chapter = document.getElementById("Chapter").value;
+  const verse = document.getElementById("Verse").value;
+  const URL = `https://bible-api.com/${book}+${chapter}:${verse}`;
+  getData(URL);
+  console.log(URL);
+});
 
 async function getData(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-document.getElementById("api-response").textContent = data.reference ;
-document.getElementById("api-response2").textContent = data.text ;
-
+    document.getElementById("api-response").textContent = data.reference;
+    document.getElementById("api-response2").textContent = data.text;
   } catch (error) {
     console.log(error);
   }
 }
 
+DOMSelectors.button2.addEventListener("click", function () {
+  const addVerse = function () {
+    const verse = document.getElementById("Verse").value;
+    this.verse++;
+  };
+});
+
+const monkey = {
+  firstName: "monkey",
+  lastName: "Monkeyson",
+  bananaCount: 25,
+  alive: true,
+  addBanana: function () {
+    //method
+    console.log(this);
+    this.bananaCount++; //adds 1 banana to banana count
+  },
+  getFullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+monkey.addBanana();
+monkey.addBanana();
+console.log(monkey.getFullName());
