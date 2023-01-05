9dbc3b981e409bd9c4919263870a525dc6856bfe
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-01-04 20:57:30 -0500

    ukjtgyvfb

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,10 +1,11 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-DOMSelectors.button.addEventListener("click", function () {
+
+DOMSelectors.button.addEventListener("click", function (e) {
   const book = document.getElementById("Name").value;
   const chapter = document.getElementById("Chapter").value;
-  const verse = document.getElementById("Verse").value;
+const verse = document.getElementById("Verse").value;
   const URL = `https://bible-api.com/${book}+${chapter}:${verse}`;
   getData(URL);
   console.log(URL);
@@ -20,28 +21,25 @@ async function getData(URL) {
     console.log(error);
   }
 }
+async function getDatas(URL2) {
+  try {
+    const response = await fetch(URL2);
+    const data = await response.json();
+    document.getElementById("api-response").textContent = data.reference;
+    document.getElementById("api-response2").textContent = data.text;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 DOMSelectors.button2.addEventListener("click", function () {
-  const addVerse = function () {
-    const verse = document.getElementById("Verse").value;
-    this.verse++;
-  };
+  const book = document.getElementById("Name").value;
+  const chapter = document.getElementById("Chapter").value;
+  const verse = parseInt(document.getElementById("Verse").value)+1;
+  const URL2 = `https://bible-api.com/${book}+${chapter}:${verse}`;
+
+  getDatas(URL2);
+  console.log(URL);
 });
 
-const monkey = {
-  firstName: "monkey",
-  lastName: "Monkeyson",
-  bananaCount: 25,
-  alive: true,
-  addBanana: function () {
-    //method
-    console.log(this);
-    this.bananaCount++; //adds 1 banana to banana count
-  },
-  getFullName: function () {
-    return `${this.firstName} ${this.lastName}`;
-  },
-};
-monkey.addBanana();
-monkey.addBanana();
-console.log(monkey.getFullName());
+
