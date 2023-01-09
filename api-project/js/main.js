7d6f5f1e293130995d563d0d428d1b5d5f2e0f36
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-01-09 00:12:59 -0500

    etjhnadu

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,49 +1,37 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-DOMSelectors.button.addEventListener("click", function () {
-  const book = document.getElementById("Name").value;
-  const chapter = document.getElementById("Chapter").value;
-  const verse = parseInt(document.getElementById("Verse").value);
-  const URL = `https://bible-api.com/${book}+${chapter}:${verse}`;
-  getData(URL);
-  bob();
+const URL = "https://seussology.info/api/books"
 
-  console.log(URL);
-});
 
-async function getData(URL) {
+function readQuote(){
+  let input = document.querySelectorAll(".output");
+  input.forEach((showQuote)=>{
+    showQuote.addEventListener("click", function(){ 
+      console.log("hi")}
+    );
+  });
+};
+
+
+async function getDatas(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    DOMSelectors.box.insertAdjacentHTML(
-      "beforeend",
-      `<div class="output">
-      <h3>${data.reference}</h3>
-       <h3>${data.text}</h3>
-       <button class="btn2">Add</button> 
-      </div>`
-    );
-  } catch (error) {
-    console.log(error);
-  }
-}
-async function getDatas(URL2) {
-  try {
-    const response = await fetch(URL2);
-    const data = await response.json();
-    document.getElementById("api-response").textContent = data.reference;
-    document.getElementById("api-response2").textContent = data.text;
+    data.forEach(element => {
+      DOMSelectors.box.insertAdjacentHTML(
+        "beforeend",
+        `<div class="output">
+        <button onclick="readQuote()">
+        <img src="${element.image}" alt="E"></button>
+        </div>`
+      );
+    });
+  
   } catch (error) {
     console.log(error);
   }
 }
 
-function bob() {
-  let button2 = document.querySelectorAll(".btn2");
-  button2.forEach((goNext) => {
-    goNext.addEventListener("click", () => {
-      console.log("hi");
-    });
-  });
-}
+
+getDatas(URL);
