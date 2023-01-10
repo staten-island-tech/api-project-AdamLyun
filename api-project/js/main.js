9403df0505a690ba0409b317cf5ecd82e3093e6f
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-01-10 15:13:03 -0500

    weqaghehb

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -3,15 +3,7 @@ import { DOMSelectors } from "./dom";
 
 const URL = "https://seussology.info/api/books";
 
-function readQuote() {
-  let input = document.querySelectorAll(".output");
-  input.forEach((showQuote) => {
-    showQuote.addEventListener("click", function () {
-      filter((wat) => wat.type.includes(input.id));
-      console.log("hi");
-    });
-  });
-}
+
 
 async function getDatas(URL) {
   try {
@@ -21,16 +13,34 @@ async function getDatas(URL) {
       DOMSelectors.box.insertAdjacentHTML(
         "beforeend",
         `<div class="output">
-        <button>
+        <button type="submit" onclick="openPopup()">
         <img src="${element.image}" alt="E"></button>
-        </div>`,
-        console.log(element.id)
+<div class="popup" id="popup">
+<h1>${element.title}</h1>
+<h2>${element.description}</h2>
+<h3>Published In ${element.year_published}</h3>
+<button type="button" onclick="closePopup()">Ok</button>
+</div>
+        </div>
+        <script>let popup= document.getElementById("popup")
+        function openPopup(){
+          popup.classList.add("open-popup")
+        }
+        function closePopup(){
+          popup.classList.remove("open-popup")
+        }</script>
+        `
+        ,
+       
       );
+     
+      
     });
-    readQuote();
   } catch (error) {
     console.log(error);
   }
 }
 
 getDatas(URL);
+
+
