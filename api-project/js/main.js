57d61ab9e3c5298db159f56812bed56b91e03204
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-02-23 11:18:07 -0500

    NO MORE 5000 LINES OF CODE

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -2,65 +2,56 @@ import { DOMSelectors } from "./dom";
 
 const URL = "https://seussology.info/api/books";
 
-
 async function getDatas(URL) {
   try {
     const response = await fetch(URL);
     const data = await response.json();
-    let count = 0;
-    let number = 0;
     data.forEach((element) => {
-      
-
       DOMSelectors.box.insertAdjacentHTML(
         "beforeend",
         `
         <div class="output">
-        <button class="hvr-grow yes ${number++}" onClick="">
-        <img  src="${element.image}" alt="E"></button>
-
-
-<div class="hide" id="english${count++}">
+        <button class="yes" >
+        <img  src="${element.image}" alt="${Math.random()}"></button>
+<div class="hide"  id="close">
 <h2>${element.title}</h2>
 <h3>Published In ${element.year_published}</h3>
 <p>${element.description}</p> 
-<button type="button" >Ok</button>
+<button type="button">Ok</button>
 </div>
-
         </div>
 `
       );
     });
   } catch (error) {
-    console.log(error); 
+    console.log(error);
   }
-} 
+}
 
 function openPopup() {
   let btn = document.querySelectorAll(".yes");
   btn.forEach(function (currentBtn) {
     currentBtn.addEventListener("click", function () {
-     
-       
-        let something = document.getElementById("english5" )
-        something.classList.add("open-popup")
-      
-      });
+      currentBtn
+        .closest(".output")
+        .querySelector(".hide")
+        .classList.add("open-popup");
     });
-  };
+  });
+}
 
-
-// function closePopup() {
-//   let popup2 = document.getElementById(mood[0]);
-//   popup2.addEventListener("click", function () {
-//     popup2.classList.remove("open-popup");
-//   });
-// }
+function closePopup() {
+  let popup2 = document.querySelectorAll("#close");
+  popup2.forEach((element) => {
+    element.addEventListener("click", function () {
+      element.classList.remove("open-popup");
+    });
+  });
+}
 
 async function yes() {
   await getDatas(URL);
   openPopup();
-
-  // closePopup();
+  closePopup();
 }
 yes();
